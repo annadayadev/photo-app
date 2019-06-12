43e95bcbee4a56bb26e06c07afe465ba06e2a454
annadayadev <ryry11adaya@gmail.com>
2019-06-12 19:32:02 +0800

    Add devise and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+  #this means in order to access the application you need to be a logged in user 
+  before_action :authenticate_user!
 end
